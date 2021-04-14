f4959697f5f3d7498806489807fb11b9d9194889
fouzia1146 <76620272+fouzia1146@users.noreply.github.com>
2021-04-15 01:40:28 +0600

    Update Game.java

--- src/main/java/com/udacity/Game.java
+++ src/main/java/com/udacity/Game.java
@@ -149,8 +149,19 @@ public class Game {
      * @return String indicating the outcome of the game: "X wins" or "O wins" or "Tie" or "None"
      */
     public String checkGameWinner(char [][]grid){
-        String result = "None";
-        //Student code goes here ...
+        if((grid[0][0] == 'x' && grid[0][1] =='x' && grid[0][2] =='x')  || (grid[1][0] == 'x' && grid [1][1] =='x' && grid [1][2] =='x')|| (grid[2][0] == 'x' && grid [2][1] =='x' && grid [2][2] =='x') || (grid[0][0] == 'x' && grid [1][0] =='x' && grid [2][0] =='x')||(grid[0][1] == 'x' && grid [1][1] =='x' && grid [2][1] =='x')||(grid[0][2] == 'x' && grid [1][2] =='x' && grid [2][2] =='x')||(grid[0][0] == 'x' && grid [1][1] =='x' && grid [2][2] =='x')||(grid[2][0] == 'x' && grid [1][1] =='x' && grid [0][2] =='x')){
+            result = "X wins";
+
+        }
+       else if((grid[0][0] == 'o' && grid[0][1] =='o' && grid[0][2] =='o')  || (grid[1][0] == 'o' && grid [1][1] =='o' && grid [1][2] =='o')|| (grid[2][0] == 'o' && grid [2][1] =='o' && grid [2][2] =='o') ||  (grid[0][0] == 'o' && grid [1][0] =='o' && grid [2][0] =='o')||(grid[0][1] == 'o' && grid [1][1] =='o' && grid [2][1] =='o')||(grid[0][2] == 'o' && grid [1][2] =='o' && grid [2][2] =='o')||(grid[0][0] == 'o' && grid [1][1] =='o' && grid [2][2] =='o')||(grid[2][0] == 'o' && grid [1][1] =='o' && grid [0][2] =='o')){
+            result = "O wins";
+
+        }
+       else if(grid[0][0]!='-' && grid[0][1]!='-' && grid[0][2]!='-' && grid[1][0]!='-' && grid[1][1]!='-' && grid[1][2]!='-' && grid[2][0]!='-' && grid[2][1]!='-' && grid[2][2]!='-' )
+       {
+           result = "Tie";
+       }
+        
         return result;
     }
 
